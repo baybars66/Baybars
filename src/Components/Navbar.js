c8d2cf7ad87bc1a41306b54f9d0d068829f51b45
baybars66 <bay66bars@gmail.com>
2021-05-31 21:10:56 +0300

    mm

---
+++ src/Components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component} from 'react'
+
+
+class Navbar extends Component {
+  render() {
+
+    return( 
+        
+
+<nav classname="navbar navbar-default">
+  <div classname="container-fluid">
+    <div classname="navbar-header">
+      <a classname="navbar-brand" href="http://budget.baybarsonal.com">WebSiteName</a>
+    </div>
+    <ul classname="nav navbar-nav">
+      <li classname="active">Home</li>
+      <li>Page 1</li>
+      <li>Page 2</li>
+      <li>Page 3</li>
+    </ul>
+  </div>
+</nav>
+
+        
+    
+
+
+    )
+
+
+  }
+
+
+
+}
+
+
+
+export default Navbar;
